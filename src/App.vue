68166fe0275ebabe8dcd788c720f1d16fc8ebc97
liuhb <liuhb@xintongdt.com>
2020-04-29 11:47:02 +0800

    11111111

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App  Are Youn OK!"/>
+    <HelloWorld msg="Welcome to Your Vue.js App  Are Youn OK  isooo!"/>
   </div>
 </template>
 
